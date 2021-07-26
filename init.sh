8fd6b4864a8487adb1ea61e5a406d2648fdafadd
aci30 <hochu_v_cosmos@mail.ru>
2021-07-26 07:12:49 -0400

    django start 1st try

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@ sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo gunicorn --bind='0.0.0.0:8080' hello:app &
+gunicorn --bind='0.0.0.0:8080' hello:app -D
+gunicorn --bind='0.0.0.0:8000' ask.wsgi -D
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
