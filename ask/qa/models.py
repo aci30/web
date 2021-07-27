be157f000a7d0e7255abb85b1d81dfec6cdbf703
aci30 <hochu_v_cosmos@mail.ru>
2021-07-27 05:55:08 -0400

    django mysql added

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,23 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.order_by('-added_at')
+	def popular(self):
+		return self.order_by('-rating')
+
 class Question(models.Model):
+	objects = QuestionManager()
 	title = models.CharField(max_length=255)
 	text = models.TextField()
-	added_at = models.DateTimeField()
-	rating = models.IntegerField()
-	author = models.CharField(max_length=255)
-	likes = models.ManyToManyField(User)
-
-class QuestionManager():
-	def new():
-		return Question.objects.order_by(added_at).desc()
-	def popular():
-		return Question.objects.order_by(rating)
-
+	added_at = models.DateTimeField(auto_now_add=True)
+	rating = models.IntegerField(default=0)
+	author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+	likes = models.ManyToManyField(User, related_name='question_likes_user')
 
 class Answer(models.Model):
 	text = models.TextField()
-	added_at = models.DateTimeField()
+	added_at = models.DateTimeField(auto_now_add=True)
 	question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
-	author = models.CharField(max_length=255)
+	author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
--- mysql_init.sh
+++ mysql_init.sh
@@ -1,4 +1,5 @@
 sudo /etc/init.d/mysql start
+mysql -u root -e "drop database if exists qa;"
 mysql -u root -e "create database qa;"
 mysql -u root -e "create user 'aci30'@'localhost' identified by 'aci30';"
 mysql -u root -e "grant all privileges on qa.* to 'aci30'@'localhost';"
